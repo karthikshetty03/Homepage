be51e9925f14317e52b9ffc1ccfa1c95f2b8f210
Karthik Shetty <ks03mh04@gmail.com>
2020-05-23 17:21:07 +0530

    Final Refactor

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const username = document.querySelector('#username');
-const password = document.querySelector('#password');
-const passwordconf = document.querySelector('#passwordconf');
 const form = document.querySelector('form');
 let MY_SECRET_API_KEY;
 const makeAreq = async () => {
-
 	var headers = {
 		accept: '*/*',
 		'Content-Type': 'application/json'
@@ -12,45 +9,23 @@ const makeAreq = async () => {
 
 	var dataString = '{ "password": "admin", "username": "admin@gmail.com"}';
 
-	return await fetch('https://indipl2020.herokuapp.com/authenticate',
-        
-        {
-		    method: 'POST',
-		    headers: headers,
-		    body: dataString
-	    })
-        .then(res => res.json())
-        .then(data =>data );
-
-
+	return await fetch('https://indipl2020.herokuapp.com/authenticate', {
+		method: 'POST',
+		headers: headers,
+		body: dataString
+	})
+		.then((res) => res.json())
+		.then((data) => data);
 };
 
 form.addEventListener('submit', async (event) => {
+
 	event.preventDefault();
-	if (password.value !== passwordconf.value) {
-		console.log('Password and Confirm passwords do not match !');
-		return;
-	}
-	for (let i = 0; i < localStorage.length; i++) {
-		let key = localStorage.key(i);
-		if (key === username.value) {
-			console.log('User exists');
-			return;
-		}
-	}
-
-    localStorage.setItem(username.value, password.value);
-    localStorage.setItem("currentUser", username.value);
-
-     MY_SECRET_API_KEY = await makeAreq();
-     MY_SECRET_API_KEY = MY_SECRET_API_KEY.token;
-     localStorage.setItem("Authorization",`Bearer ${MY_SECRET_API_KEY}`);
-     window.location.href = "ipl.html";
-
-
+	localStorage.setItem('currentUser', username.value);
+	console.log(username.value);
+	MY_SECRET_API_KEY = await makeAreq();
+	MY_SECRET_API_KEY = MY_SECRET_API_KEY.token;
+	localStorage.setItem('Authorization', `Bearer ${MY_SECRET_API_KEY}`);
+    window.location.href = 'ipl.html';
     
-    
-
 });
-
-
