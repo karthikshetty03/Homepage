701a23cbcde4c5f342ef0b55027cf01fd9a292aa
Karthik Shetty <ks03mh04@gmail.com>
2020-05-24 11:49:57 +0530

    Nested fetch requests

--- teamstats.js
+++ teamstats.js
@@ -5,6 +5,7 @@ if (!localStorage.getItem('currentUser')) {
 
 User.innerText = `Hello, ${localStorage.getItem('currentUser')}`;
 var amount = new Map();
+
 fetch('https://indipl2020.herokuapp.com/ipl2020/team/totalamount', {
 	headers: {
 		Authorization: localStorage.getItem('Authorization')
@@ -16,99 +17,106 @@ fetch('https://indipl2020.herokuapp.com/ipl2020/team/totalamount', {
 			const { teamName, amount } = key;
 			window.amount.set(teamName, amount);
 		});
-	});
 
-fetch('https://indipl2020.herokuapp.com/ipl2020/team/all', {
-	headers: {
-		Authorization: localStorage.getItem('Authorization')
-	}
-})
-	.then((res) => res.json())
-	.then((data) => {
-		console.log(data);
-		let j = 0;
-		for (let key of data) {
-			var td = [];
-			for (let i = 0; i < 5; i++) td[i] = document.createElement('td');
-			const { coach, home, label, team } = key;
-
-			td[0].innerText = label;
-			td[1].innerText = team;
-			td[2].innerText = coach;
-			td[3].innerText = home;
-			td[4].innerText = window.amount.get(label);
-
-			for (let i = 0; i < 5; i++) arr[j].appendChild(td[i]);
-
-			j++;
-		}
-
-		var ctx = document.getElementById('myChart');
-		ctx.height = 330;
-		var myChart = new Chart(ctx, {
-			type: 'bar',
-			data: {
-				labels: [ 'DC', 'RCB', 'MI', 'CSK', 'RR', 'KKR', 'SRH', 'KXIP' ],
-				datasets: [
-					{
-						label: 'Total amount spent on all players',
-						data: [
-							760000000,
-							786000000,
-							830500000,
-							848500000,
-							702500000,
-							765000000,
-							749000000,
-							645000000
-						],
-						backgroundColor: [ 'green', 'red', 'blue', 'yellow', 'orange', 'aqua', 'purple', 'teal' ],
-						borderColor: 'black',
-						borderWidth: 1,
-						hoverBorderWidth: 5
-					}
-				]
-			},
-			options: {
-					tooltips: {
-						mode: 'nearest'
-					},
-				scales: {
-					yAxes: [
-						{
-							ticks: {
-								beginAtZero: true,
-								fontSize: 30,
-								fontColor: 'black'
-							},
-						gridLines: {
-								color: 'black'
-							}
-						
-
-						}
-					],
-					xAxes: [
-						{
-							ticks: {
-								beginAtZero: true,
-								fontSize: 30,
-								fontColor: 'black'
-							},
-							gridLines: {
-								color: 'black'
-							}
-						}
-					]
-				},
-				legend: {
-					labels: {
-						fontSize: 30,
-						fontColor: 'black'
-					}
-				}
+		fetch('https://indipl2020.herokuapp.com/ipl2020/team/all', {
+			headers: {
+				Authorization: localStorage.getItem('Authorization')
 			}
-		});
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				let j = 0;
+				for (let key of data) {
+					var td = [];
+					for (let i = 0; i < 5; i++) td[i] = document.createElement('td');
+					const { coach, home, label, team } = key;
+
+					td[0].innerText = label;
+					td[1].innerText = team;
+					td[2].innerText = coach;
+					td[3].innerText = home;
+					td[4].innerText = window.amount.get(label);
+
+					for (let i = 0; i < 5; i++) arr[j].appendChild(td[i]);
+
+					j++;
+				}
+
+				var ctx = document.getElementById('myChart');
+				ctx.height = 330;
+				var myChart = new Chart(ctx, {
+					type: 'bar',
+					data: {
+						labels: [ 'DC', 'RCB', 'MI', 'CSK', 'RR', 'KKR', 'SRH', 'KXIP' ],
+						datasets: [
+							{
+								label: 'Total amount spent on all players',
+								data: [
+									window.amount.get('DC'),
+									window.amount.get('RCB'),
+									window.amount.get('MI'),
+									window.amount.get('CSK'),
+									window.amount.get('RR'),
+									window.amount.get('KKR'),
+									window.amount.get('SRH'),
+									window.amount.get('KXIP')
+								],
+								backgroundColor: [
+									'green',
+									'red',
+									'blue',
+									'yellow',
+									'orange',
+									'aqua',
+									'purple',
+									'teal'
+								],
+								borderColor: 'black',
+								borderWidth: 1,
+								hoverBorderWidth: 5
+							}
+						]
+					},
+					options: {
+						tooltips: {
+							mode: 'nearest'
+						},
+						scales: {
+							yAxes: [
+								{
+									ticks: {
+										beginAtZero: true,
+										fontSize: 30,
+										fontColor: 'black'
+									},
+									gridLines: {
+										color: 'black'
+									}
+								}
+							],
+							xAxes: [
+								{
+									ticks: {
+										beginAtZero: true,
+										fontSize: 30,
+										fontColor: 'black'
+									},
+									gridLines: {
+										color: 'black'
+									}
+								}
+							]
+						},
+						legend: {
+							labels: {
+								fontSize: 30,
+								fontColor: 'black'
+							}
+						}
+					}
+				});
+			});
 	});
 
 //Utility function to delte all children of an element
